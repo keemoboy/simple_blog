e47f2e86284f6ad462c893ccfbb36acfbe49eb1f
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-06 11:24:54 -0800

    before filter in controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
+  before_filter :find, :only => [:edit, :update, :destroy]
+
   def index
     @articles = Article.all
 
@@ -34,12 +36,12 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
-    @article = Article.find(params[:id])
   end
 
   # POST /articles
   # POST /articles.json
   def create
+
     @article = Article.new(params[:article])
 
     respond_to do |format|
@@ -56,7 +58,6 @@ class ArticlesController < ApplicationController
   # PUT /articles/1
   # PUT /articles/1.json
   def update
-    @article = Article.find(params[:id])
 
     respond_to do |format|
       if @article.update_attributes(params[:article])
@@ -72,12 +73,17 @@ class ArticlesController < ApplicationController
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
 
     respond_to do |format|
       format.html { redirect_to articles_url }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def find
+    @article = Article.find(params[:id])
   end
 end
